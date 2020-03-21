81af0dbac63f27836392c0be463bfd4940ed7841
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-03-21 10:55:06 +0100

    Add DI test

---
+++ YahooFinance.Core/YahooFinance.Core.Tests/DI/DependencyInjectionTests.cs
@@ -0,0 +1,45 @@
+﻿using MatthiWare.YahooFinance.Core;
+using MatthiWare.YahooFinance.Core.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace YahooFinance.Tests.DI
+{
+    public class DependencyInjectionTests
+    {
+        private readonly ITestOutputHelper testOutput;
+
+        public DependencyInjectionTests(ITestOutputHelper testOutput)
+        {
+            this.testOutput = testOutput;
+        }
+
+        [Fact]
+        public void TestDISetup()
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder => 
+            {
+                builder.ClearProviders();
+                builder.AddXunit(testOutput);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            services.AddYahooFinance();
+
+            var provider = services.BuildServiceProvider();
+
+            var client = provider.GetService<IYahooFinanceClient>();
+
+            Assert.NotNull(client);
+            Assert.NotNull(client.Search);
+            Assert.NotNull(client.Quote);
+            Assert.NotNull(client.History);
+
+            provider.Dispose();
+        }
+    }
+}
